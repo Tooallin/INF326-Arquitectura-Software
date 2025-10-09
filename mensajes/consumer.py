535e9d36cc7663ad858d43c7716ecba244b88e4f
JanJorquera <janjorquera@gmail.com>
2025-10-09 16:39:12 -0300

    [Fix] falta de importacion de conexión con elastic search

--- mensajes/consumer.py
+++ mensajes/consumer.py
@@ -1,6 +1,9 @@
 from mensajes.mapping import index_name
+from elastic_search.connection import get_client
 
 def create(body: dict):
+    es = get_client()
+    
     try:
         es.index(index=index_name, id=body["id"], document=body)
     except Exception as e:
