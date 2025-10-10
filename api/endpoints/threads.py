2d786919420e6ab508f9b33f5dda99fb90510c7d
José Astudillo <jose.astudillo@usm.cl>
2025-10-10 01:42:52 -0300

    Cambio mínimo

--- api/endpoints/threads.py
+++ api/endpoints/threads.py
@@ -1,33 +1,3 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 
-app = FastAPI()
-
-
-class Thread(BaseModel):
-    id: int
-    title: str | None = None
-    content: float
-    author_id: float | None = None
-    creation_date:
-    tags:
-    category:
-
-    model_config = {
-        "json_schema_extra": {
-            "examples": [
-                {
-                    "name": "Foo",
-                    "description": "A very nice Item",
-                    "price": 35.4,
-                    "tax": 3.2,
-                }
-            ]
-        }
-    }
-
-
-@app.put("/items/{item_id}")
-async def update_item(item_id: int, item: Item):
-    results = {"item_id": item_id, "item": item}
-    return results
