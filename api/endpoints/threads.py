56754f830f004d350beafe37ce08a2dcc7a8a24d
JanJorquera <janjorquera@gmail.com>
2025-10-10 14:25:12 -0300

    [Fix] errores varios

--- api/endpoints/threads.py
+++ api/endpoints/threads.py
@@ -1,3 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter
 from pydantic import BaseModel
 
+router = APIRouter()
--- threads/mapping.py
+++ threads/mapping.py
@@ -1,19 +1,21 @@
 index_name = "threads"
 
 #Definir la estructura
-mappings = {
-    "properties":{
-        "id": { "type": "keyword" },
-        "title":{ 
-            "type": "text",
-            "fields":{
-                "keyword": { "type": "keyword", "ignore_above": 256 }
-            }
-                  } ,
-        "content": { "type": "text" }, 
-        "author_id": { "type": "keyword" },
-        "creation_date": { "type": "date" } ,
-        "tags": { "type": "keyword" },
-        "category": { "type": "keyword" }
+mapping = {
+    "mappings": {
+        "properties":{
+            "id": { "type": "keyword" },
+            "title":{ 
+                "type": "text",
+                "fields":{
+                    "keyword": { "type": "keyword", "ignore_above": 256 }
+                }
+                    } ,
+            "content": { "type": "text" }, 
+            "author_id": { "type": "keyword" },
+            "creation_date": { "type": "date" } ,
+            "tags": { "type": "keyword" },
+            "category": { "type": "keyword" }
+        }
     }
 }
