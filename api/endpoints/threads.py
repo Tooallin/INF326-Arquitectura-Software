38720fd1e7915a3c2f9b1d200bcc651d5132db5b
Josecris10 <jose.astudillom@usm.cl>
2025-10-13 14:04:39 -0300

    endpoints threads

--- api/endpoints/threads.py
+++ api/endpoints/threads.py
@@ -1,4 +1,42 @@
 from fastapi import APIRouter, FastAPI
 from pydantic import BaseModel
+from datetime import datetime
+from typing import Optional
+from threads.services import *
 
 router = APIRouter()
+
+class Thread(BaseModel):
+    id: str
+    title: str
+    content: str
+    author_id: str
+    channel_id: str
+    creation_date: datetime
+    tags: Optional[list[str]] = None
+    category: Optional[str] = None
+
+
+@router.get("/id/{thread_id}", response_model=Thread)
+def read_id(thread_id: str):
+    return get_by_id(thread_id)
+
+@router.get("/category/{thread_category}", response_model=Thread)
+def read_category(thread_category: str):
+    return get_by_category(thread_category)
+
+@router.get("/author/{thread_author}", response_model=Thread)
+def read_author(thread_author: str):
+    return get_by_author(thread_author)
+
+@router.get("/daterange", response_model=Thread)
+def read_date_range(start_date: datetime, end_date: datetime):
+    return get_by_date_range(start_date, end_date)
+
+@router.get("/tag/{thread_tag}", response_model=Thread)
+def read_tag(thread_tag: str):
+    return get_by_tag(thread_tag)
+
+@router.get("/keyword/{thread_keyword}")
+def read_keyword(thread_keyword: str):
+    return get_by_keyword(thread_keyword)
--- threads/services.py
+++ threads/services.py
@@ -1,73 +1,84 @@
 from elastic_search.connection import get_client
-from elasticsearch.dsl import Search, query, aggs
 from threads.mapping import index_name
 
 def get_by_id(thread_id):
-    client=get_client()
-    s = Search(using=client, index=index_name).query(query.Term("id",thread_id))
-    
-    response = s.execute()
-    
-    results = [hit.to_dict() for hit in response]
-
+    client = get_client()
+    body = {
+        "query": {
+            "term": {
+                "id": thread_id
+            }
+        }
+    }
+    response = client.search(index=index_name, body=body)
+    results = [hit["_source"] for hit in response["hits"]["hits"]]
     return results        
     
 def get_by_category(category):
-    client=get_client()
-    s = Search(using=client, index=index_name) \
-        .query(query.Term("category", category))
-    
-    response = s.execute()
-
-    results = [hit.to_dict() for hit in response]
-    
+    client = get_client()
+    body = {
+        "query": {
+            "term": {
+                "category": category
+            }
+        }
+    }
+    response = client.search(index=index_name, body=body)
+    results = [hit["_source"] for hit in response["hits"]["hits"]]
     return results
 
 def get_by_author(author_id):
-    client=get_client()
-    s= Search(using=client, index=index_name) \
-        .query(query.Term("author_id", author_id))
-    
-    response = s.execute()
-
-    results = [hit.to_dict() for hit in response]
-    
+    client = get_client()
+    body = {
+        "query": {
+            "term": {
+                "author_id": author_id
+            }
+        }
+    }
+    response = client.search(index=index_name, body=body)
+    results = [hit["_source"] for hit in response["hits"]["hits"]]
     return results
 
 def get_by_date_range(start_date, end_date):
-    client=get_client()
-    s= Search(using=client, index=index_name) \
-        .query(query.Range(
-            creation_date={"gte": start_date, "lt":end_date}
-        ))    
-    response = s.execute()
-
-    results = [hit.to_dict() for hit in response]
-    
+    client = get_client()
+    body = {
+        "query": {
+            "range": {
+                "creation_date": {
+                    "gte": start_date,
+                    "lt": end_date
+                }
+            }
+        }
+    }
+    response = client.search(index=index_name, body=body)
+    results = [hit["_source"] for hit in response["hits"]["hits"]]
     return results
 
 def get_by_tag(tag):
-    client=get_client()
-    s = Search(using=client, index=index_name) \
-        .query(query.Term("tags", tag))
-    
-    response = s.execute()
-
-    results = [hit.to_dict() for hit in response]
-    
+    client = get_client()
+    body = {
+        "query": {
+            "term": {
+                "tags": tag
+            }
+        }
+    }
+    response = client.search(index=index_name, body=body)
+    results = [hit["_source"] for hit in response["hits"]["hits"]]
     return results
 
 def get_by_keyword(keyword):
-    client=get_client()
-    s = Search(using=client, index=index_name) \
-        .query(
-            query.MultiMatch(
-            query=keyword,
-            fields=["title", "content", "tags", "category"],
-            )
-        )
-    
-    response = s.execute()
-
-    results = [hit.to_dict() for hit in response]
+    client = get_client()
+    body = {
+        "query": {
+            "multi_match": {
+                "query": keyword,
+                "fields": ["title", "content", "tags", "category"]
+            }
+        }
+    }
+    response = client.search(index=index_name, body=body)
+    results = [hit["_source"] for hit in response["hits"]["hits"]]
     return results
