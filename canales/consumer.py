ec03ff7813fad187905b5c6993d1584e919fd34d
JanJorquera <janjorquera@gmail.com>
2025-10-16 22:06:20 -0300

    [Add] consumer de channels

--- canales/consumer.py
+++ canales/consumer.py
@@ -12,13 +12,13 @@ def create(body: dict):
 def update(body: dict):
     es = get_client()
     channel_id = body.get("id")
-    if not message_id:
+    if not channel_id:
         raise ValueError("El cuerpo debe incluir un campo 'id' para actualizar el documento.")
 
     try:
         es.update(
             index=index_name,
-            id=message_id,
+            id=channel_id,
             body={"doc": body}  # 👈 se actualizan solo los campos presentes
         )
     except Exception as e:
@@ -30,11 +30,11 @@ def delete(body: dict):
     Requiere que body contenga el campo 'id'.
     """
     es = get_client()
-    message_id = body.get("id")
-    if not message_id:
+    channel_id = body.get("id")
+    if not channel_id:
         raise ValueError("El cuerpo debe incluir un campo 'id' para eliminar el documento.")
 
     try:
-        es.delete(index=index_name, id=message_id)
+        es.delete(index=index_name, id=channel_id)
     except Exception as e:
         raise
