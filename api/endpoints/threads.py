0e219c4d7e520bb7e8f1fb9272a9dadd3d5409dd
JanJorquera <janjorquera@gmail.com>
2025-10-18 18:51:43 -0300

    fix de no se que

--- api/endpoints/threads.py
+++ api/endpoints/threads.py
@@ -16,26 +16,26 @@ class Thread(BaseModel):
     tags: Optional[list[str]] = None
     category: Optional[str] = None
 
-@router.get("/id/{thread_id}", response_model=List[Thread])
+@router.get("/id/{thread_id}")
 def read_id(thread_id: str):
     return get_by_id(thread_id)
 
-@router.get("/category/{thread_category}", response_model=List[Thread])
+@router.get("/category/{thread_category}")
 def read_category(thread_category: str):
     return get_by_category(thread_category)
 
-@router.get("/author/{thread_author}", response_model=List[Thread])
+@router.get("/author/{thread_author}")
 def read_author(thread_author: str):
     return get_by_author(thread_author)
 
-@router.get("/daterange", response_model=List[Thread])
+@router.get("/daterange")
 def read_date_range(start_date: datetime, end_date: datetime):
     return get_by_date_range(start_date, end_date)
 
-@router.get("/tag/{thread_tag}", response_model=List[Thread])
+@router.get("/tag/{thread_tag}")
 def read_tag(thread_tag: str):
     return get_by_tag(thread_tag)
 
-@router.get("/keyword/{thread_keyword}", response_model=List[Thread])
+@router.get("/keyword/{thread_keyword}")
 def read_keyword(thread_keyword: str):
     return get_by_keyword(thread_keyword)
