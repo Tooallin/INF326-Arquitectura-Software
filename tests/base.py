aa4390d1e2c70f43bd533d9681b67a8858aa23d1
JanJorquera <janjorquera@gmail.com>
2025-11-20 16:49:45 -0300

    av

--- tests/base.py
+++ tests/base.py
@@ -2,5 +2,6 @@ from fastapi.testclient import TestClient
 
 from main import app
 
-base_url = "https://searchservice.inf326.nursoft.dev/api"
+base_url = "/api"
+base_remote_url = "https://searchservice.inf326.nursoft.dev/api"
 client = TestClient(app)
